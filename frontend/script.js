7f37607f9036928af73e94ba1204499e741f0820
porneczy <porneczy@gmail.com>
2022-02-03 16:02:11 +0100

    addtwointegers

---
+++ frontend/script.js
@@ -0,0 +1,15 @@
+function addTwoIntegers(firstInteger, secondInteger)
+{
+    //console.log(firstInteger + secondInteger)
+    //return firstInteger + secondInteger;
+    //return "hello";
+    if(typeof firstInteger === "number" && typeof secondInteger === "number"){
+        return firstInteger + secondInteger
+    }else{
+        return "valamelyik argumentum nem szám ezért nem elvégezhető";
+    }
+}
+
+console.log( addTwoIntegers(12, 13) );
+console.log( addTwoIntegers(1920, 85) );
+console.log(0==="0");
